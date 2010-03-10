155477b950783d5caf075eeb7c4a7aaab5a0be35
Peter Williams <peter.williams.97@gmail.com>
2010-03-10 13:39:48 +1100

    added face_common.h

---
+++ face_common.h
@@ -0,0 +1,14 @@
+/*
+ *  face_common.h
+ *  FaceTracker
+ *
+ *  Created by peter on 4/03/10.
+ *
+ */
+
+#ifdef NOT_MAC_APP
+ #include "cv.h"
+ #include "highgui.h"
+#else
+ #include <OpenCV/OpenCV.h>
+#endif
